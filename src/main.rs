4a0c403475446d1aad9f496980d42b2f4782ed02
Scufftistic <lmckenna090@gmail.com>
2024-01-04 17:36:40 -0500

    Added very simple movement system. Player moves based off enum, MoveState.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+mod player_movement;
 
 use bevy::prelude::*;
+use player_movement::*;
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins);
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup, spawn_character))
+        .add_systems(Update, (player_move))
+        .run();
 }
